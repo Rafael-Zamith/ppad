c1a8123b9d5ff53e6867f5bbd893087ae75fd5f5
Rafael-Zamith <31914454@mackenzista.com.br>
2021-05-07 14:32:25 -0300

    busca aprimorada
    funciona com mais de um item

--- site/TmDB.js
+++ site/TmDB.js
@@ -2,13 +2,18 @@ var basep = "https://api.themoviedb.org/3/search/multi?api_key=f79172df98ee2e8bc
 var basei = "https://image.tmdb.org/t/p/w185";
 var pesquisa = "";
 var title = "";
+var final = true;
+var resultados;
 
 function search(ele) {
     if (event.key === 'Enter') {
-        pesquisa = ele.value;
-        pesquisa = encodeURI(pesquisa);
-        pesquisa = basep + pesquisa;
-        buscarPorNome();
+        if (final == true) {
+            final = false;
+            pesquisa = ele.value;
+            pesquisa = encodeURI(pesquisa);
+            pesquisa = basep + pesquisa;
+            buscarPorNome();
+        }
     }
 }
 function buscarPorId(id) {
@@ -17,14 +22,47 @@ function buscarPorId(id) {
 function buscarPorNome() {
     fetch(pesquisa)
         .then(response => response.json())
-        .then(data => pegarimagem(data.results[0].poster_path));
+        .then(data => separarResultados(data));
 }
 
-function pegarimagem(idm) {
-    var urll = basei + String(idm);
-    var x = document.createElement("IMG");
-    x.setAttribute("src", urll);
-    x.setAttribute("alt", title);
-    document.body.appendChild(x);
+
+function separarResultados(data) {
+    resultados = data.results;
+    for (let i = 0; i < resultados.length; i++){
+        var url = "";
+        pegarimagem(resultados[i].poster_path, i);
+        if (resultados[i].media_type == "movie") { url = "bucar filme" }
+        if (resultados[i].media_type == "tv") { url = "bucar serie" }
+        pegarDescFilme(url); //precisa de outro fech
+    }
+}
+
+function pegarDescFilme(url) {
+    var desc = desc;
+    var url = url;
+
+    //dom para descrição
+}
+
+
+function pegarimagem(idm, index) {
+    if (idm != null) {
+        var id = "img" + String(index);
+        var urll = basei + String(idm);
+        var x = document.createElement("IMG"); //mudar isso para trocar o src da imagem
+        x.setAttribute("src", urll);
+        x.setAttribute("alt", title);
+        final = true;
+        document.body.appendChild(x);
+        final = true;
+    }
+    else {
+        var x = document.createElement("IMG"); //mudar isso para trocar o src da imagem
+        x.setAttribute("src", "imagens/noimage.png");
+        x.setAttribute("alt", title);
+        final = true;
+        document.body.appendChild(x);
+        final = true;
+    }
 }
 
