2759b977c43592aefe6e881a36773bfcea61fb87
Rafael-Zamith <31914454@mackenzista.com.br>
2021-05-14 21:57:29 -0300

    padronizando DOM

--- site/item.js
+++ site/item.js
@@ -24,7 +24,47 @@ function onload() {
     z = document.getElementById("1");
     if (id[2] == "f") { pegarTitulo(item.title) };
     if (id[2] == "s") { pegarTitulo(item.name) };
-    pegarimagem(item);
+    pegarimage(item);
     pegarDesc(item.overview);
     document.body.appendChild(z);
+}
+
+function pegarimage(idm) {
+    var url = "";
+    if (idm.poster_path != null) {
+        url = basei + String(idm.poster_path);
+        var x = document.getElementById("3"); //mudar isso para trocar o src da imagem
+        x.setAttribute("src", url);
+        x.setAttribute("id", idm.id + "=" + idm.media_type);
+        x.setAttribute("onClick", "abrirElemento(this.id)");
+        //x.setAttribute("href", link);
+        x.setAttribute("alt", title);
+        final = true;
+        z.appendChild(x);
+        final = true;
+    } else {
+        var x = document.getElementById("3"); //mudar isso para trocar o src da imagem
+        x.setAttribute("src", "imagens/noimage.png");
+        x.setAttribute("id", idm.id + "=" + idm.media_type);
+        x.setAttribute("onClick", "abrirElemento(this.id)");
+        //x.setAttribute("href", link);
+        x.setAttribute("alt", title);
+        final = true;
+        z.appendChild(x);
+        final = true;
+    }
+}
+function pegarDes(desc) { //não usado por enquanto
+    if (desc != null) {
+        var y = document.getElementById("4"); //mudar isso para trocar o texto
+        var t = document.createTextNode(desc);
+        y.appendChild(t);
+        z.appendChild(y);
+    } else {
+        var y = document.getElementById("4");
+        var t = document.createTextNode("sem descrição");
+        y.appendChild(t);
+        z.appendChild(y);
+    }
+
 }
