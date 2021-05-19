3fcc34bac1ac542ad00aa09b62fa4eca412ea687
Rafael-Zamith <31914454@mackenzista.com.br>
2021-05-19 19:23:08 -0300

    aaa

--- site/coment.js
+++ site/coment.js
@@ -11,7 +11,7 @@ function postar(comentario) {
             id_item: id
         };
         console.log(data);
-        fetch("https://pisana.duckdns.org/:8080/avaliacao/", {
+        fetch("https://pisana.duckdns.org:8080/avaliacao/", {
             method: "POST",
                 body: JSON.stringify(data)
         }).then(res => {
